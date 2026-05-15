0e7ab41556e184aed5b52de90a7327882db9f320
Mohamed irfan <mohdirfanr0329@gmail.com>
2026-05-15 15:05:27 +0530

    Delete Downloads/shdesignmeld projects/projects/open/ytplayer directory

--- Downloads/shdesignmeld projects/projects/open/ytplayer/server/src/routes/musicRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { searchMusic, getTrending } from '../controllers/musicController';
-
-const router = express.Router();
-
-router.get('/search', searchMusic);
-router.get('/trending', getTrending);
-
-export default router;
